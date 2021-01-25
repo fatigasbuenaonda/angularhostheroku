8f06d19d5705e1a34851992a918fde5dcdd2b47b
Quallio, Juan Facundo <juan.facundo.quallio@accenture.com>
2021-01-25 16:40:44 -0300

    qwert

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/finsa'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/finsa/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/finsa/index.html'));
+});
+app.listen(process.env.PORT || 1111);
